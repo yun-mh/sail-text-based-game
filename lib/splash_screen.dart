cee69598074127615c3250e2999d65b1c4446ed6
Minho Yun <minhoyun.dev@gmail.com>
2021-09-13 10:21:55 +0900

    Create splash screen

---
+++ lib/splash_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
+
+import 'package:sail/main.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    bool lightMode =
+        MediaQuery.of(context).platformBrightness == Brightness.light;
+
+    return AnimatedSplashScreen(
+      backgroundColor: lightMode ? Colors.white : Color.fromRGBO(12, 12, 12, 1),
+      splash: lightMode
+          ? Image.asset(
+              'assets/images/splash_logo_light.png',
+            )
+          : Image.asset(
+              'assets/images/splash_logo_dark.png',
+            ),
+      splashIconSize: 150,
+      nextScreen: Main(),
+      splashTransition: SplashTransition.fadeTransition,
+      pageTransitionType: PageTransitionType.fade,
+    );
+  }
+}
